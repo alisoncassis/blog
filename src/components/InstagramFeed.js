a0cf9f0fafd2b7d8f2e72ac08e903daa5f965d9a
alisoncassisn <alisoncassisn@gmail.com>
2020-06-20 16:41:42 -0300

    netlify error with envs

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -28,6 +28,8 @@ export default class InstagramFeed extends Component {
   }
 
   componentDidMount() {
+    console.log(process.env)
+    console.log(JSON.parse(process.env))
     this.clearStorage()
     if (!this.state.mounted) {
       this.fetchInstagram()
